924ef74e4bfb9e1d3905f8c97f862ed3ea458032
Luis Fernando Molina Antequera <flmolina@uninorte.edu.co>
2023-03-02 09:44:30 -0500

    changes on main

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import numpy as np
 import pyaudio
 import struct
 import classes_n_functions as cnf
+import time
 
 # Configuración de la grabación
 FORMAT = pyaudio.paInt16  # Formato de audio
 CHANNELS = 1  # Número de canales (1 para mono, 2 para estéreo)
 fs = 48000  # Tasa de muestreo (muestras por segundo)
-N = 1024*2 # Número de muestras por bloque
+N = 350*2 # Número de muestras por bloque
 
 
 ##Parametros del sistema
@@ -27,7 +28,7 @@ stream = p.open(format=FORMAT,
                 rate=fs,
                 input=True,
                 frames_per_buffer=N,
-                input_device_index=1)
+                input_device_index=11)
 
 fig, ax= plt.subplots()
 ax.set_ylim(0,600)
@@ -37,11 +38,14 @@ x=np.arange(0,int(N/2+1),1)
 line,=ax.plot(x,np.random.rand(int(N/2+1)))
 fig.show()
 while True:
+    inicio=time.time()
     data= stream.read(N,exception_on_overflow=False)
     data_int= struct.unpack(str(N)+"h",data)
 
     FFT=cnf.True_FFT(data_int,N)
-    print(cnf.data_Mag(FFT))
+    ab=(cnf.data_Mag(FFT))
     line.set_ydata(abs(FFT))
     fig.canvas.draw()
     fig.canvas.flush_events()
+    final=time.time()
+    print((final-inicio)*1000)
