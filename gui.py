a799e286e6f12716bcb384dec84118d6e04be6df
Luis Fernando Molina Antequera <flmolina@uninorte.edu.co>
2024-03-10 18:24:32 -0500

    AVANCES 10 MARZO

--- gui.py
+++ gui.py
@@ -10,14 +10,15 @@ Phase_A=clnf.phase(110,0.5,0,0)
 Phase_B=clnf.phase(110,0.5,-120,-120)
 Phase_C=clnf.phase(110,0.5,120,120)
 
+I_DISPARO=100
 
 
-def actualizar_etiquetas(A,B,C):
+def actualizar_etiquetas(A,B,C,D):
     # Aquí puedes definir la lógica para actualizar el contenido de las etiquetas
     etiqueta1.config(text=A)
     etiqueta2.config(text=B)
     etiqueta3.config(text=C)
-
+    etiqueta4.config(text=D)
 def catch():
     global Phase_A
     FORMAT = pyaudio.paInt16  # Formato de audio
@@ -44,11 +45,14 @@ def catch():
         FFT=clnf.True_FFT(data_int,N)
 
         Phase_A.I=round(clnf.data_Mag(FFT,CICLOS),3)
-
+       
         etiqueta1.config(text=Phase_A.mostrar_atributos())
         etiqueta2.config(text=Phase_B.mostrar_atributos())
         etiqueta3.config(text=Phase_C.mostrar_atributos())
-        
+        if Phase_A.I>I_DISPARO:
+            etiqueta4.config(text="FALLA")
+        else:
+            etiqueta4.config(text="NO FALLA")
         final=time.time()
 
         #print((final-inicio)*1000)
@@ -58,7 +62,7 @@ def catch():
 ## ------------Estetica de la interfaz-------------  ##
 ventana = tk.Tk()
 ventana.title("Ejemplo de GUI con Tkinter")
-ventana.configure(bg="#69D36C")
+ventana.configure(bg="#5585D1")
 # Establecer las dimensiones de la ventana (ancho x alto)
 ancho_ventana = 1024
 alto_ventana = 600
@@ -76,11 +80,14 @@ font_style = ("FixedSys", 20)  # Cambiar la fuente y el tamaño aquí
 etiqueta1 = tk.Label(ventana, text="Etiqueta 1", font=font_style, width=20, height=5)
 etiqueta2 = tk.Label(ventana, text="Etiqueta 2", font=font_style, width=20, height=5)
 etiqueta3 = tk.Label(ventana, text="Etiqueta 3", font=font_style, width=20, height=5)
+etiqueta4= tk.Label(ventana, text="Etiqueta 3", font=font_style, width=20, height=5)
+
 
 # Ubicar las etiquetas en la ventana
 etiqueta1.grid(row=1, column=1, sticky="nsew")
 etiqueta2.grid(row=1, column=2, sticky="nsew")
 etiqueta3.grid(row=1, column=3, sticky="nsew")
+etiqueta4.grid(row=2, column=2, sticky="nsew")
 #--------------------------------------------------------------------------#
 ## ------------Operacion señales------------  ##
 
